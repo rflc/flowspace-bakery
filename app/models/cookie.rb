1fa8196095bbbc13f5df6064575b75ecd26e040d
Rafael Carabano <rafael@sftwrsolutions.com>
2021-11-17 01:45:54 -0500

    Feature: Added worker to update cookie cooked state

--- app/models/cookie.rb
+++ app/models/cookie.rb
@@ -2,4 +2,8 @@ class Cookie < ActiveRecord::Base
   belongs_to :storage, polymorphic: :true
   
   validates :storage, presence: true
+  def ready?
+    # return self.cooked
+    return true
+  end
 end
